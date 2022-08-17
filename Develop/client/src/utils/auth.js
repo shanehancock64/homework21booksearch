9a258362d4fd2ee783fca25b05364892812791a3
Michael Hancock <shanehancock64@live.com>
2022-08-17 15:01:21 -0700

    changing server from rest to apollo

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,6 +1,6 @@
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
-
+const jwt = require('jsonwebtoken');
 // create a new class to instantiate for a user
 class AuthService {
   // get user data
