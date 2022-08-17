d512e0a64cd6eafebe04157a790a693fa682285e
Michael Hancock <shanehancock64@live.com>
2022-08-17 14:47:07 -0700

    Update App.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,6 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
 
 function App() {
   return (
