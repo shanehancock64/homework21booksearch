84b80650e0a74663e23189f8d76e23c88e91f2b7
Michael Hancock <shanehancock64@live.com>
2022-08-17 14:56:21 -0700

    Update index.js

--- Develop/server/routes/index.js
+++ Develop/server/routes/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
 
-// serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-});
+module.exports = router;
 
 module.exports = router;
